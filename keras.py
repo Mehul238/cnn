d594ba42cdb2ea5b41ee3de53c4c6185f5a1f2e0
Mehul238 <55505628+Mehul238@users.noreply.github.com>
2020-05-22 11:44:42 +0530

    Update keras.py

--- keras.py
+++ keras.py
@@ -27,6 +27,9 @@ from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 # Creating a Sequential Model and adding the layers
 model = Sequential()
 model.add(Conv2D(20, kernel_size=(3,3), input_shape=input_shape))
+
+
+
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten()) # Flattening the 2D arrays for fully connected layers
 model.add(Dense(100, activation=tf.nn.relu))
